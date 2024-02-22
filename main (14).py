33b24a6674fe2874d7817475bbc2718d6dcb351e
killklitor <158448112+killklitor@users.noreply.github.com>
2024-02-22 11:08:07 +0300

    Add files via upload
    22.02.2024 практические задания 1-5

---
+++ main (14).py
@@ -0,0 +1,25 @@
+#coding uft-8
+#Задача Nº1
+x = int(input ("Введите х - "))
+y - 7*x+5
+print (y)
+#Задача Nº2
+q = int (input ("Введите q = "))
+w = q*4
+print (w)
+#Задача Nº3
+p = 3.14
+r = int (input ("Введите г = "))
+e = (p 2) *r
+print (e)
+#Задача Nº4
+i = 3.14
+d = int (input ("Введите d = "))
+R=d/2
+u = iRR
+print (u)
+#Задача Nº5
+a = int(input ("Введите а І "))
+b = int (input ("Введите b = ") )
+c+*c= a*a + b**b
+print (c)
