f45dad8202f953abd8a017f8f3a98cf5b1ac50eb
killklitor <158448112+killklitor@users.noreply.github.com>
2024-02-29 11:38:08 +0300

    29.02.2024 Практические задание 11-15
    29.02.2024 Практические задание 11-15

---
+++ main.py
@@ -0,0 +1,35 @@
+#Задание 11. 
+#m = float(input("Введите массу"))
+#v = float(input("Введите объем"))
+#p = m / v 
+#print(p)
+
+#Задание 12. 
+#a = float(input())
+#b = float(input())
+#c = a + b
+#d = a - b 
+#e = a * b
+#f = a / b 
+#print(c, d, e, f)
+
+#Задание 13
+#a = float(input())
+#b = float(input())
+#c = (a + b) / 2
+#d = (a * b) ** 0.5
+#print(c,d)
+
+#Задание 14
+#a = float(input("введите площадь"))
+#b = float(input("введите кол-во жителей"))
+#c = b / a 
+#print (c) 
+
+#Задание 15
+#a = float(input())
+#b = float(input())
+#с = float(input())
+#s = 2 * (a * b + b * c + c * a )
+#v - a * b * c
+#print(s,v)
