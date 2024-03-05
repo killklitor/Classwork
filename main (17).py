ff0d9ad7cef4572498d6654b82a4d6e8d6ddd832
killklitor <158448112+killklitor@users.noreply.github.com>
2024-03-05 10:26:59 +0300

    05.03.2024 практическая работа 1-5
    05.03.2024 практическая работа 1-5

---
+++ main (17).py
@@ -0,0 +1,38 @@
+#Задание 1
+#p = float(input())
+#a = 2
+#while a < p :
+    #print (a)
+    #a += 1 
+    
+#Задание 2
+#a = float(input())
+#b = float(input())
+#while a < b:
+    #a += 1
+    #print (a)
+
+#Задание 3. 
+#a = float(input())
+#b = float(input())
+#c = a + 1
+#while c < b:
+    #b -= 1 
+    #print(b)
+    
+#Задание 4
+#n = float(input())
+#k = 1 
+#while 3*k <= n:
+    #k += 1 
+#k -= 1 
+#f = 3*k 
+#prinr (f,k)
+
+#Задание 5. 
+#n = float(input())
+#k = 1 
+#while 3 * k < n:
+    #k+=1 
+#k -= 1 
+#print (k)
