ed7a77938cc26511390d55d452d7c62047466c2c
killklitor <158448112+killklitor@users.noreply.github.com>
2024-03-05 10:17:32 +0300

    05.03.2024 практическая работа 1-5
    05.03.2024 практическая работа 1-5

--- main (16).py
+++ main (16).py
@@ -1,40 +1,37 @@
-#coding: utf-8
-#задание 1 
-a = int(input("Введите первое число"))
-b = int(input("Введите второе число"))
-if a > b:
-    maximus = a
-    minimus = b
-else:
-    maximus = b
-    minimus = a
-print("Максимум", maximus)
-print("Минимум", minimus)      
+#Задание 1
+#p = float(input())
+#a = 2
+#while a < p :
+    #print (a)
+    #a += 1 
+    
 #Задание 2
-a = int(input("Введите сторону квадрата"))
-b = int(input("Введите радиус круга"))
-if 2*b <= a:
-    print("Круг впишется в квадрат")
-else:
-    print("Круг не впишется в квадрат")
-#Задание 3
-a = int(input("Введите число"))
-if a > 0:
-    b = 1/a*a 
-else:
-    b = a*a 
-print (b)
+#a = float(input())
+#b = float(input())
+#while a < b:
+    #a += 1
+    #print (a)
+
+#Задание 3. 
+#a = float(input())
+#b = float(input())
+#while b > a:
+    #print (b)
+    #b -= 1
+    
 #Задание 4
-a = int(input("Введите сторону квадрата"))
-b = int(input("Введите радиус круга"))
-if b * 2 => a:
-    print('квадрат впишется в круг')
-else:
-     print('квадрат не впишется в круг')
-#задача 5
-a = int(input("Введите первое вещественное число"))
-a = int(input("Введите второе вещественное число"))
-if a > b:
-    print("первое вещественное число больше второго")
-else:
-     print("второе вещественное число больше второго")
+#n = float(input())
+#k = 1 
+#while 3*k <= n:
+    #k += 1 
+#k -= 1 
+#f = 3*k 
+#prinr (f,k)
+
+#Задание 5. 
+#n = float(input())
+#k = 1 
+#while 3 * k < n:
+    #k+=1 
+#k -= 1 
+#print (k)
